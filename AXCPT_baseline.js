899903db5771c1e7d8754163324ed583b6b41b27
Urahara-san <yurk282@gmail.com>
2026-06-11 17:45:40 +0300

    Update AXCPT_baseline.js

--- AXCPT_baseline.js
+++ AXCPT_baseline.js
@@ -2721,8 +2721,8 @@ function block_breakRoutineEachFrame() {
         
         // Run 'Each Frame' code from code
         if ((t >= 60)) {
-            text.text = "\u041f\u0435\u0440\u0435\u0440\u044b\u0432 \u043e\u043a\u043e\u043d\u0447\u0435\u043d.\n\n\u041d\u0430\u0436\u043c\u0438\u0442\u0435 \u041f\u0420\u041e\u0411\u0415\u041b \u0434\u043b\u044f \u043f\u0440\u043e\u0434\u043e\u043b\u0436\u0435\u043d\u0438\u044f.";
-        }
+    text.setText("Минута прошла.\n\nНажмите ПРОБЕЛ, если готовы продолжить, либо отдохните ещё несколько минут (не более 5).");
+}
         
         
         // *key_resp_5* updates
